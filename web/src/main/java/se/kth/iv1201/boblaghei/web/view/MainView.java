13fa6d3cdbca358ab790376aa09a3a226a05c69b
Andreas Lagerström <alage@kth.se>
2018-01-23 15:12:33 +0100

    Add index page

---
+++ web/src/main/java/se/kth/iv1201/boblaghei/web/view/MainView.java
@@ -0,0 +1,13 @@
+package se.kth.iv1201.boblaghei.web.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainView {
+
+    @RequestMapping
+    public String mainView(){
+        return "index";
+    }
+}
