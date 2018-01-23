eb398d57a1cb00015493d8921ce71199ef3416b2
Andreas Lagerström <alage@kth.se>
2018-01-23 14:02:27 +0100

    Initial commit

---
+++ personservice/src/main/java/se/kth/iv1201/boblaghei/personservice/PersonserviceApplication.java
@@ -0,0 +1,12 @@
+package se.kth.iv1201.boblaghei.personservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonserviceApplication.class, args);
+	}
+}
---
+++ src/main/java/se/kth/iv1201/boblaghei/architecturecourseproject/ArchitectureCourseProjectApplication.java
@@ -0,0 +1,12 @@
+package se.kth.iv1201.boblaghei.architecturecourseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureCourseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureCourseProjectApplication.class, args);
+	}
+}
