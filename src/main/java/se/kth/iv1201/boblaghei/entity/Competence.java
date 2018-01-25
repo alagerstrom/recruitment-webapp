08944c1c748ac7774f4ce40092c17f6806222032
Sebastian Heimlén <sebastian@heimlen.se>
2018-01-25 14:45:36 +0100

    Remove .idea

--- src/main/java/se/kth/iv1201/boblaghei/entity/Competence.java
+++ src/main/java/se/kth/iv1201/boblaghei/entity/Competence.java
@@ -1,7 +1,11 @@
 package se.kth.iv1201.boblaghei.entity;
 
+import javax.persistence.Id;
+
 /**
  *  O/R Mapping of the table Competence in the database.
  */
 public class Competence {
+    @Id
+    private long Id;
 }
