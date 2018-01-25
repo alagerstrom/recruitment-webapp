7f45a83db949288cea77e74a5eaa5bfe298b205d
Andreas Lagerström <alage@kth.se>
2018-01-25 13:42:48 +0100

    Change project structure

---
+++ boblaghei/src/main/java/se/kth/iv1201/boblaghei/view/MainView.java
@@ -0,0 +1,14 @@
+package se.kth.iv1201.boblaghei.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainView {
+
+    @RequestMapping
+    public String mainView(){
+        // Mycket konstigt
+        return "index";
+    }
+}
---
+++ boblaghei/src/test/java/se/kth/iv1201/boblaghei/AppTests.java
@@ -0,0 +1,16 @@
+package se.kth.iv1201.boblaghei;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
