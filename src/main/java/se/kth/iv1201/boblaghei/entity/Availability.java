49a5f30480fbb5d85d0ee0f8d87e5b77075dcb22
Sebastian Heimlén <sebastian@heimlen.se>
2018-01-25 15:26:35 +0100

    All entities created.

--- src/main/java/se/kth/iv1201/boblaghei/entity/Availability.java
+++ src/main/java/se/kth/iv1201/boblaghei/entity/Availability.java
@@ -1,7 +1,65 @@
 package se.kth.iv1201.boblaghei.entity;
 
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  *  O/R Mapping of the table Availability in the database.
  */
+@Entity
 public class Availability {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private Date from;
+
+    @Column(nullable = false)
+    private Date to;
+
+    @Column(nullable = false)
+    @ManyToOne(cascade= CascadeType.ALL)
+    private Person person;
+
+    public Availability() {
+    }
+
+    public Availability(Date from, Date to, Person person) {
+        this.from = from;
+        this.to = to;
+        this.person = person;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getFrom() {
+        return from;
+    }
+
+    public void setFrom(Date from) {
+        this.from = from;
+    }
+
+    public Date getTo() {
+        return to;
+    }
+
+    public void setTo(Date to) {
+        this.to = to;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
 }
--- src/main/java/se/kth/iv1201/boblaghei/entity/CompetenceProfile.java
+++ src/main/java/se/kth/iv1201/boblaghei/entity/CompetenceProfile.java
@@ -1,8 +1,66 @@
 package se.kth.iv1201.boblaghei.entity;
 
+import javax.persistence.*;
 
 /**
  *  O/R Mapping of the table CompetenceProfile in the database.
  */
+@Entity
 public class CompetenceProfile {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private double yearsOfExperience;
+
+    @Column(nullable = false)
+    @ManyToOne(cascade=CascadeType.ALL)
+    private Person person;
+
+    @Column(nullable = false)
+    @ManyToOne(cascade=CascadeType.ALL)
+    private Competence competence;
+
+    public CompetenceProfile() {
+    }
+
+    public CompetenceProfile(double yearsOfExperience, Person person, Competence competence) {
+        this.yearsOfExperience = yearsOfExperience;
+        this.person = person;
+        this.competence = competence;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(double yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Competence getCompetence() {
+        return competence;
+    }
+
+    public void setCompetence(Competence competence) {
+        this.competence = competence;
+    }
 }
