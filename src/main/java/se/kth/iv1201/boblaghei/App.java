368d9ea664cfc77bda7829a18254cc4acb226bdf
Andreas Lagerström <alage@kth.se>
2018-01-25 13:47:38 +0100

    Change project structure

---
+++ src/main/java/se/kth/iv1201/boblaghei/App.java
@@ -0,0 +1,12 @@
+package se.kth.iv1201.boblaghei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
