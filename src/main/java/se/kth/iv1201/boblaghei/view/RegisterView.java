d685fe2334a16d9e74e3be6ccc25e0021a021bd2
Sebastian Heimlén <sebastian@heimlen.se>
2018-01-29 12:59:06 +0100

    resolved conflicts

--- src/main/java/se/kth/iv1201/boblaghei/view/RegisterView.java
+++ src/main/java/se/kth/iv1201/boblaghei/view/RegisterView.java
@@ -5,23 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import se.kth.iv1201.boblaghei.dto.PersonDto;
-import se.kth.iv1201.boblaghei.dto.UserDto;
+import se.kth.iv1201.boblaghei.dto.PersonDTO;
+import se.kth.iv1201.boblaghei.dto.UserDTO;
 
 @Controller
 public class RegisterView {
 
     @GetMapping("/register")
     public String getRegisterView(Model model){
-        UserDto user = new UserDto();
-        PersonDto person = new PersonDto();
+        UserDTO user = new UserDTO();
+        PersonDTO person = new PersonDTO();
         person.setUser(user);
         model.addAttribute("person", person);
         return "register";
     }
 
     @PostMapping("/register")
-    public String postRegistration(@ModelAttribute PersonDto person, Model model){
+    public String postRegistration(@ModelAttribute PersonDTO person, Model model){
         System.out.println("I should register " + person);
         return "index";
 
