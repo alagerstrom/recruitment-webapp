57c153020b021eb84bc16c68f6dac2a403714c8e
Sebastian Heimlén <sebastian@heimlen.se>
2018-02-05 13:48:52 +0100

    fixed the test that was bugging

--- src/main/java/se/kth/iv1201/boblaghei/repository/CompetenceProfileRepository.java
+++ src/main/java/se/kth/iv1201/boblaghei/repository/CompetenceProfileRepository.java
@@ -1,8 +1,12 @@
 package se.kth.iv1201.boblaghei.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import se.kth.iv1201.boblaghei.entity.Application;
 import se.kth.iv1201.boblaghei.entity.CompetenceProfile;
+
+import java.util.List;
 
 public interface CompetenceProfileRepository extends CrudRepository<CompetenceProfile, Long> {
 
+    List<CompetenceProfile> getCompetenceProfilesByApplication(Application application);
 }
