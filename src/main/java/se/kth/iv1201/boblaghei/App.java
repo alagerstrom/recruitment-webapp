b93bcb45de9dee2c7421ab8367dd973b0ed057d2
Andreas Lagerström <alage@kth.se>
2018-02-07 16:46:34 +0100

    Add basic functionality for localisation

--- src/main/java/se/kth/iv1201/boblaghei/App.java
+++ src/main/java/se/kth/iv1201/boblaghei/App.java
@@ -2,21 +2,40 @@ package se.kth.iv1201.boblaghei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 /**
  * Main method that starts the application
  */
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
+public class App extends WebMvcConfigurerAdapter {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(App.class);
-	}
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return sessionLocaleResolver;
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
--- src/main/java/se/kth/iv1201/boblaghei/view/MainView.java
+++ src/main/java/se/kth/iv1201/boblaghei/view/MainView.java
@@ -1,14 +1,12 @@
 package se.kth.iv1201.boblaghei.view;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import se.kth.iv1201.boblaghei.dto.PersonDTO;
 import se.kth.iv1201.boblaghei.exception.NoUserLoggedInException;
 import se.kth.iv1201.boblaghei.service.RegisterService;
-import se.kth.iv1201.boblaghei.service.SecurityService;
 
 /**
  * Controller responsible for providing mappings used for initial opening of application.
@@ -20,9 +18,14 @@ public class MainView {
     RegisterService registerService;
 
     @GetMapping("/")
-    public String getMainView(Model model) throws NoUserLoggedInException {
-        PersonDTO personDTO = registerService.getLoggedInPerson();
-        model.addAttribute(personDTO);
+    public String getWelcomeView(Model model) {
+
+        PersonDTO personDTO = null;
+        try {
+            personDTO = registerService.getLoggedInPerson();
+            model.addAttribute(personDTO);
+        } catch (NoUserLoggedInException ignored) {
+        }
         return "index";
     }
 }
