846a1172c32302ce45257a65cb09661b27d2e68c
peterboberg <peterb@frost.se>
2018-02-15 15:16:48 +0100

    [DEV] Add back navigation from view single application

--- src/main/java/se/kth/iv1201/boblaghei/util/DateUtil.java
+++ src/main/java/se/kth/iv1201/boblaghei/util/DateUtil.java
@@ -1,5 +1,7 @@
 package se.kth.iv1201.boblaghei.util;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
@@ -8,9 +10,14 @@ import java.util.Date;
  * Helper class that eases usage of java.util.Date
  */
 public class DateUtil {
-    public static Date getDateFrom(LocalDate localDate){
+    public static Date getDateFrom(LocalDate localDate) {
         if (localDate == null)
             return null;
         return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static String formatAsDateString(Date date) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return dateFormat.format(date);
     }
 }
